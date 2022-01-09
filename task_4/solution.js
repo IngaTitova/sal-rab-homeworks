fe81613133e31958f9d91880a18eebfb0c794155
IngaTitova <itg13@yandex.ru>
2022-01-09 20:45:47 +0500

    Extra String Deleted

--- task_4/solution.js
+++ task_4/solution.js
@@ -4,8 +4,7 @@ function parseProducts(json) {
     // Аргументом функции является JSON
 
     let data = JSON.parse(json);
-    
-    
+        
     return data.products;// Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
