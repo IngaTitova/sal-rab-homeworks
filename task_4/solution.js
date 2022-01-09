0e9c4e843b39dd9c1713a992c77abf7c10835df9
IngaTitova <itg13@yandex.ru>
2022-01-09 20:26:17 +0500

    Fourth Task is corrected

--- task_4/solution.js
+++ task_4/solution.js
@@ -16,12 +16,14 @@ function parseProducts(json) {
 
 function renderProductsCards(json) {
     clearProducts();
-    let products;
-    products = parseProducts(json);
-    length = products.length;
-    for (let i = 0, i < length - 1, i += 1) {
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length - 1; i ++) {
         addProduct(products[i]);
-    }// Напишите функцию renderProductsCards(json)
+    }
+    return products;
+}
+// Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
